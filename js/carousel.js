9799c9c6d75111b568b3280c3d0953d145ecec6e
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-09 21:44:40 +0100

    function carouselAutomatic

--- js/carousel.js
+++ js/carousel.js
@@ -1,35 +1,56 @@
+const mycarousel = document.querySelectorAll('.carousel')
+const indicator = document.querySelectorAll('.dot')
+
 
 let indexCarousel = 1
 
 carousel(indexCarousel)
+carouselAuto()
 
-function plusCarousel(n){
+function plusCarousel(n) {
   carousel(indexCarousel += n)
 }
 
-function indicatorThumbinail(n){
+function indicatorThumbinail(n) {
   carousel(indexCarousel = n)
 }
 
-function carousel(n){
+function carousel(n) {
 
-  const mycarousel = document.querySelectorAll('.carousel')
-  const indicator = document.querySelectorAll('.dot')
 
-  if(indexCarousel > mycarousel.length){indexCarousel = 1}
-  if(indexCarousel < 1){indexCarousel = mycarousel.length}
+  if (n > mycarousel.length) {
+    indexCarousel = 1
+  }
+  if (n < 1) {
+    indexCarousel = mycarousel.length
+  }
 
   let i
-  for(i = 0; i < mycarousel.length; i++){
+  for (i = 0; i < mycarousel.length; i++) {
     //mycarousel[i].classList.add('carousel')
     mycarousel[i].style.display = 'none'
   }
-  for(i = 0; i < indicator.length; i++){
+  for (i = 0; i < indicator.length; i++) {
     indicator[i].classList.remove('active-dot')
   }
-  
-  //mycarousel[indexCarousel-1].classList.remove('carousel');
-  mycarousel[indexCarousel-1].style.display = 'block'
-  indicator[indexCarousel-1].classList.toggle('active-dot')
 
+  //mycarousel[indexCarousel-1].classList.remove('carousel');
+  mycarousel[indexCarousel - 1].style.display = 'block'
+  indicator[indexCarousel - 1].classList.toggle('active-dot')
+
+}
+
+function carouselAuto() {
+
+  let i
+
+  for(i = 0; i < mycarousel.length; i++){
+    mycarousel[i].style.display = 'none'
+  }
+  if(indexCarousel > mycarousel.length){ indexCarousel = 1}
+
+  mycarousel[indexCarousel-1].style.display = 'block'
+
+  indexCarousel++
+  setTimeout(carouselAuto, 3500)
 }
