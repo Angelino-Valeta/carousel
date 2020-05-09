eebe2563206e1a56ffe7c68828db559dc9ee2362
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-09 20:46:07 +0100

    up carousel

--- js/carousel.js
+++ js/carousel.js
@@ -16,9 +16,13 @@ function carousel(n){
   const mycarousel = document.querySelectorAll('.carousel')
   const indicator = document.querySelectorAll('.dot')
 
+  if(indexCarousel > mycarousel.length){indexCarousel = 1}
+  if(indexCarousel < 1){indexCarousel = mycarousel.length}
+
   let i
   for(i = 0; i < mycarousel.length; i++){
-    mycarousel[i].classList.add('carousel')
+    //mycarousel[i].classList.add('carousel')
+    mycarousel[i].style.display = 'none'
   }
   for(i = 0; i < indicator.length; i++){
     indicator[i].classList.remove('active-dot')
