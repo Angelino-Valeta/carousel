7ae980529c93f8de7d8a942ab3ab6cfe57e8b7f3
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-09 20:26:30 +0100

    add in carousel indicator

--- js/carousel.js
+++ js/carousel.js
@@ -6,12 +6,16 @@ carousel(indexCarousel)
 function carousel(n){
 
   const mycarousel = document.querySelectorAll('.carousel')
+  const indicator = document.querySelectorAll('.dot')
 
   let i
   for(i = 0; i < mycarousel.length; i++){
     mycarousel[i].classList.add('carousel')
   }
+  for(i = 0; i < indicator.length; i++){
+    indicator[i].classList.remove('active-dot')
+  }
   mycarousel[indexCarousel-1].classList.toggle('carousel');
-
+  indicator[indexCarousel-1].classList.toggle('active-dot')
 
 }
