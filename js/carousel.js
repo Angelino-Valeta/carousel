3e0c4b70844d0bb70156692644a3e61af6153a13
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-09 22:02:34 +0100

    up function carouselAuto

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,3 @@
-const mycarousel = document.querySelectorAll('.carousel')
-const indicator = document.querySelectorAll('.dot')
-
-
 let indexCarousel = 1
 
 carousel(indexCarousel)
@@ -11,12 +7,41 @@ function plusCarousel(n) {
   carousel(indexCarousel += n)
 }
 
+
+function carouselAuto() {
+
+  const mycarousel = document.querySelectorAll('.carousel')
+  const indicator = document.querySelectorAll('.dot')
+
+  let i
+
+  for (i = 0; i < mycarousel.length; i++) {
+    mycarousel[i].style.display = 'none'
+  }
+  for (i = 0; i < indicator.length; i++) {
+    indicator[i].classList.remove('active-dot')
+  }
+
+  if (indexCarousel > mycarousel.length) {
+    indexCarousel = 1
+  }
+
+  mycarousel[indexCarousel - 1].style.display = 'block'
+  indicator[indexCarousel - 1].classList.toggle('active-dot')
+
+  indexCarousel++
+  setTimeout(carouselAuto, 3000)
+}
+
+
 function indicatorThumbinail(n) {
   carousel(indexCarousel = n)
 }
 
 function carousel(n) {
 
+  const mycarousel = document.querySelectorAll('.carousel')
+  const indicator = document.querySelectorAll('.dot')
 
   if (n > mycarousel.length) {
     indexCarousel = 1
@@ -40,17 +65,3 @@ function carousel(n) {
 
 }
 
-function carouselAuto() {
-
-  let i
-
-  for(i = 0; i < mycarousel.length; i++){
-    mycarousel[i].style.display = 'none'
-  }
-  if(indexCarousel > mycarousel.length){ indexCarousel = 1}
-
-  mycarousel[indexCarousel-1].style.display = 'block'
-
-  indexCarousel++
-  setTimeout(carouselAuto, 3500)
-}
